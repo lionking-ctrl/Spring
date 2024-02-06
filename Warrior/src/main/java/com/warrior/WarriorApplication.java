ba73ca8f4597c9b4899a7a5c24b3867d5c8c831e
lionking-ctrl <157677868+lionking-ctrl@users.noreply.github.com>
2024-02-06 22:12:20 +0530

    Warrior
    Warrior

---
+++ Warrior/src/main/java/com/warrior/WarriorApplication.java
@@ -0,0 +1,14 @@
+package com.warrior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarriorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarriorApplication.class, args);
+	}
+	//2
+
+}
